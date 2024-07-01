874b049ea58bd11f176c75d2d70f303cb94174fd
Priyanshu rajak <rajakpriyanshu581@gmail.com>
2024-07-01 12:53:07 +0530

    use onChnage event

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,14 +1,26 @@
+import { useState } from "react";
 import "./AddTask.css";
 export const AddTask = () => {
+  const [task, setTask] = useState("");
+
+  const handleAddTask = (event) => {
+    setTask(event.target.value);
+  };
   return (
     <section className="addtask">
       <form>
-        <label htmlFor="task">Task</label>
-        <input type="text" id="task" required placeholder="Add Task" />
+        <input
+          onChange={handleAddTask}
+          type="text"
+          id="task"
+          required
+          placeholder="Add Task"
+        />
         <label htmlFor="date">Date</label>
         <input type="date" id="date" required />
         <button type="submit">Add Task</button>
       </form>
+      <p>{task}</p>
     </section>
   );
 };
