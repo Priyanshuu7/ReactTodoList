0cdf31690cb26bf685790c55996b8aba88348529
Priyanshu rajak <rajakpriyanshu581@gmail.com>
2024-08-16 12:17:28 +0530

    Update Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,9 +3,7 @@ import "./Footer.css";
 export const Footer = () => {
   return (
     <footer>
-      <p>2030 - TaskMate</p>
-      <p>Copyright &copy; 2022</p>
-      <p>All Rights Reserved</p>
+      <p>2030 - TaskMate Copyright &copy; 2022 All Rights Reserved</p>
     </footer>
   );
 };
